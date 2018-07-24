1f1ee1ec3a5f62c6d37bce5f5b6b798f3b7282fc
Ben <menubar@protonmail.com>
2018-07-24 04:11:58 -0400

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+const { PATH, LAMBDA_TASK_ROOT } = process.env
+const path = require('path')
+process.env.PATH = PATH + ':' + path.resolve(LAMBDA_TASK_ROOT, __dirname, './lib')
+
+module.exports.version = () => {
+  require('child_process').exec(
+    'pdftk --version',
+    (error, stdout, stderr) => {
+      if (error) reject(error)
+      else resolvee(stdout)
+    })
+}
