e361b15c99a7cec61f840d0d00f21188a0dc3ea1
Dominik <inikdom@users.noreply.github.com>
2019-10-31 17:22:55 -0400

    Update index.js
    added append for ld_library

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { PATH, LAMBDA_TASK_ROOT } = process.env
+const { PATH, LAMBDA_TASK_ROOT, LD_LIBRARY_PATH } = process.env
 
 const BIN = 'node_modules/pdftk-lambda/bin'
 
 process.env.PATH = `${PATH}:${LAMBDA_TASK_ROOT}/${BIN}`
-process.env.LD_LIBRARY_PATH = `${LAMBDA_TASK_ROOT}/${BIN}`
+if(LD_LIBRARY_PATH)
+  process.env.LD_LIBRARY_PATH = `${LD_LIBRARY_PATH}:${LAMBDA_TASK_ROOT}/${BIN}`
+else
+  process.env.LD_LIBRARY_PATH = `${LAMBDA_TASK_ROOT}/${BIN}`
+
 process.env.PKG_CONFIG_PATH = `${LAMBDA_TASK_ROOT}/${BIN}`
 
 module.exports.version = () => {
